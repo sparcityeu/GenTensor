d5a68b304eba6a91693ab0c0fe251a0ad4c32618
tuztorun <ttorun@ku.edu.tr>
2024-01-14 08:42:19 +0300

    generate rand2 edited

--- generate_random2.sh
+++ generate_random2.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-# ./genten_naive_rand 6186 24 77 -i 32 -n 6131557 -o ../data_tensors_random2/chicago-crime-comm_random2.tns;
-# ./genten_naive_rand 6185 24 380 -i 395 -j 32 -n 13956859 -o ../data_tensors_random2/chicago-crime-geo_random2.tns;
-# ./genten_naive_rand 532924 17262471 2480308 -n 208276745 -o ../data_tensors_random2/delicious-3d_random2.tns;
-# ./genten_naive_rand 6066 5699 244268 -i 1176 -n 194804192 -o ../data_tensors_random2/enron_random2.tns;
-# ./genten_naive_rand 319686 28153045 1607191 -n 150103281 -o ../data_tensors_random2/flickr-3d_random2.tns;
-# ./genten_naive_rand 319686 28153045 1607191 -i 731 -n 253440400 -o ../data_tensors_random2/flickr-4d_random2.tns;
-# ./genten_naive_rand 23343790 23344784 166 -n 1016594327 -o ../data_tensors_random2/freebase_music_random2.tns;
-# ./genten_naive_rand 1605 4198 1631 -i 4209 -j 868131 -n 18695616 -o ../data_tensors_random2/lbnl-network_random2.tns;
-# ./genten_naive_rand 12092 9184 28818 -n 90074635 -o ../data_tensors_random2/nell2_random2.tns;
-# ./genten_naive_rand 2482 2862 14036 -i 17 -n 6703214 -o ../data_tensors_random2/nips_random2.tns;
-# ./genten_naive_rand 183 24 1140 -i 1717 -n 4121714 -o ../data_tensors_random2/uber_random2.tns;
-# ./genten_naive_rand 165427 11374 2 -n 29161958 -o ../data_tensors_random2/vast-2015-mc1-3d_random2.tns;
-# ./genten_naive_rand 165427 11374 2 -i 100 -j 89 -n 57676861 -o ../data_tensors_random2/vast-2015-mc1-5d_random2.tns;
-./genten_naive_rand	532924 17262471	2480308	-i 1443 -n 180893854 -o	../data_tensors_random2/delicious-4d_random2.tns;
+
+./genten_naive_rand 22476 22476 23776223 -n 28397869 -o ../data_tensors_random2/1998darpa.tns;
+./genten_naive_rand 532924 17262471 2480308 -n 139994992 -o ../data_tensors_random2/delicious-3d.tns;
+./genten_naive_rand 319686 28153045 1607191 -n 113726748 -o ../data_tensors_random2/flickr-3d.tns;
+./genten_naive_rand 23343790 23344784 166 -n 102919918 -o ../data_tensors_random2/freebase_music.tns;
+./genten_naive_rand 38954435 38955429 532 -n 145810929 -o ../data_tensors_random2/freebase_sampled.tns;
+./genten_naive_rand 2902330 2143368 25495389 -n 145154088 -o ../data_tensors_random2/nell1.tns;
+./genten_naive_rand 12092 9184 28818 -n 76204397 -o ../data_tensors_random2/nell2.tns;
+./genten_naive_rand 165427 11374 2 -n 25800183 -o ../data_tensors_random2/vast-2015-mc1-3d.tns;
+./genten_naive_rand 6186 24 77 -i 32 -n 5131221 -o ../data_tensors_random2/chicago-crime-comm.tns;
+./genten_naive_rand 6066 5699 244268 -i 1176 -n 57021427 -o ../data_tensors_random2/enron.tns;
+./genten_naive_rand 319686 28153045 1607191 -i 731 -n 113641030 -o ../data_tensors_random2/flickr-4d.tns;
+./genten_naive_rand 2482 2862 14036 -i 17 -n 3040652 -o ../data_tensors_random2/nips.tns;
+./genten_naive_rand 183 24 1140 -i 1717 -n 3283148 -o ../data_tensors_random2/uber.tns;
+./genten_naive_rand 6185 24 380 -i 395 -j 32 -n 6364316 -o ../data_tensors_random2/chicago-crime-geo.tns;
+./genten_naive_rand 1605 4198 1631 -i 4209 -j 868131 -n 1784327 -o ../data_tensors_random2/lbnl-network.tns;
+./genten_naive_rand 165427 11374 2 -i 100 -j 89 -n 26021841 -o ../data_tensors_random2/vast-2015-mc1-5d.tns;
 
